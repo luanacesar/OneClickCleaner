e058c1bdc34c9f0b53987cc8b8888ebd1faa90ff
Jhennifer Cardozo <jstumpfc@my.centennialcollege.ca>
2020-11-09 23:25:55 -0400

    R05

--- OCC/OCC/Startup.cs
+++ OCC/OCC/Startup.cs
@@ -15,6 +15,7 @@ namespace OCC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Jhennifer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,7 @@ namespace OCC
                 app.UseDeveloperExceptionPage();
             }
             //Raphael create
+            
             app.UseMvcWithDefaultRoute();
         }
     }

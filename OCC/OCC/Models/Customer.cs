e0af79492590cbe3b6d6b8b5f2fb277d9e3346de
cai <czhan103@my.centennialcollege.ca>
2020-11-11 18:38:46 -0500

    forgot adding the new file

---
+++ OCC/OCC/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace OCC.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Emeil { get; set; }
+        public string Phone { get; set; }
+    }
+}

1cf3717bc88fc158b64e4e60549a1009380cecda
Andrea Pineda Cabezas <apiendac@my.centennialcollege.ca>
2020-11-12 19:45:40 -0500

    Service Class Creation

--- OCC/OCC/Models/Order.cs
+++ OCC/OCC/Models/Order.cs
@@ -11,10 +11,18 @@ namespace OCC.Models
 
         public long CustomerId { get; set; }
 
-        public string Location { get; set; }
-        public int Duration { get; set; }
-        public int ShiftTime { get; set; }
-        public string OrderPaymentState { get; set; }
+        public long CleanerId { get; set; }
+
         public long ServiceId { get; set; }
+
+        public string Location { get; set; }
+
+        public int Duration { get; set; }
+       
+        //ShiftTime: Morning, Afternoon, Evening, Night
+        public string ShiftTime { get; set; }
+
+        public string OrderPaymentState { get; set; }
+        
     }
 }

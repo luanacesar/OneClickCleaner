27320d288ed0d7cce5c0d9c41fe0346bd0d953f2
Jorge Rojas Rios <jrojasri@my.centennialcollege.ca>
2020-11-19 01:52:55 -0500

    Task 4,  US5

--- OCC/OCC/Models/Cleaner.cs
+++ OCC/OCC/Models/Cleaner.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OCC.Models
 {
-    public class Cleaner 
+    public class Cleaner
     {
         public long CleanerId { get; set; }
 
         public string FirstName { get; set; }
-
+        
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Location { get; set; }
@@ -18,7 +20,10 @@ namespace OCC.Models
         public bool IsCleaner { get; set; }
 
         public string ExperienceLevel { get; set; }
-
+        
+        //[MinLength(7)]
+        //[MaxLength(8)]
+        [RegularExpression("^[0-9]{7,8}$", ErrorMessage = "Write Valid Certificate (7 to 8 digits)")]
         public string Certificate { get; set; }
 
         public long BankAccount { get; set; }

c01f4e7a89cf51b0c1a9ee8cc323f7393d7478b2
luanacesar <lcesarde@my.centennialcollege.ca>
2020-11-19 14:17:05 -0500

    conflicts

---
+++ OCC/OCC/Models/Cleaner.cs
@@ -0,0 +1,29 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace OCC.Models
+{
+    public class Cleaner
+    {
+        public long CleanerId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public bool IsCleaner { get; set; }
+        [Required]
+        public string ExperienceLevel { get; set; }
+        [Required]
+        public string Certificate { get; set; }
+        [Required]
+        public double BankAccount { get; set; }
+        
+
+    }
+}
+

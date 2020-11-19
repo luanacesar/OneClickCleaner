8d7c9d3045d89f1b3fcbdd4b6389639222777a4e
Jhennifer Cardozo <jstumpfc@my.centennialcollege.ca>
2020-11-19 00:15:30 -0400

    Design - button

--- OCC/OCC/Migrations/20201119040022_Initial.Designer.cs
+++ OCC/OCC/Migrations/20201119040022_Initial.Designer.cs
@@ -10,14 +10,14 @@ using OCC.Models;
 namespace OCC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20201119000528_initial")]
-    partial class initial
+    [Migration("20201119040022_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.11-servicing-32099")
+                .HasAnnotation("ProductVersion", "2.1.14-servicing-32113")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 

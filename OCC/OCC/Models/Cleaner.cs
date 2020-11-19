8c9fb7f72f6dd94badb09258791eb80a725ba297
Al Ajlan Majrehe Lien <alien1@my.centennialcollege.ca>
2020-11-19 10:03:59 -0500

    updating

--- OCC/OCC/Models/Cleaner.cs
+++ OCC/OCC/Models/Cleaner.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace OCC.Models
 {
-    public class Cleaner
+    public class Cleaner 
     {
         public long CleanerId { get; set; }
-
-        public string FullName { get; set; }
-
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
         public string Location { get; set; }
-
+        [Required]
+        public bool IsCleaner { get; set; }
+        [Required]
         public string ExperienceLevel { get; set; }
+        [Required]
+        public string Certificate { get; set; }
+        [Required]
+        public long BankAccount { get; set; }
+
     }
 }

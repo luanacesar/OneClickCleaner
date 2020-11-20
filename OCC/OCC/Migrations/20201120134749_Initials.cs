dff02677d036b3315a44f904fa5298611b1d89dc
rmc3408 <rmc3408@protonmail.com>
2020-11-20 08:50:57 -0500

    final commit to US7

--- OCC/OCC/Migrations/20201120134749_Initials.cs
+++ OCC/OCC/Migrations/20201120134749_Initials.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace OCC.Migrations
 {
-    public partial class Initial : Migration
+    public partial class Initials : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {

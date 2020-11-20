5d2b55063d0ab9db95abe4dfb8a59d6bedf2bc34
Raphael Molinaro <64808456+rmc3408@users.noreply.github.com>
2020-11-20 08:33:19 -0500

    Add files via upload
    parts1 till model

--- OCC/OCC/Models/Cleaner.cs
+++ OCC/OCC/Models/Cleaner.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace OCC.Models
 {
     public class Cleaner
     {
         public long CleanerId { get; set; }
-
-        public string FullName { get; set; }
-
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
         public string Location { get; set; }
-
+        [Required]
+        public bool IsCleaner { get; set; }
+        [Required]
         public string ExperienceLevel { get; set; }
+        [RegularExpression("^[0-9]{7,8}$", ErrorMessage = "Write Valid Certificate (7 to 8 digits)")]
+        public string Certificate { get; set; }
+        
+        [Required]
+        public double BankAccount { get; set; }
+        
+
     }
 }
+

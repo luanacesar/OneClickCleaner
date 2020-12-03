f760ed7e10a05306d7f2a213fd5f538bfb01838e
luanacesar <63056960+luanacesar@users.noreply.github.com>
2020-12-02 23:21:23 -0500

    Update UsersController.cs
    -  Edit the Profile page cleaner
    -  Edit button

--- OCC/OCC/Controllers/UsersController.cs
+++ OCC/OCC/Controllers/UsersController.cs
@@ -30,6 +30,68 @@ namespace OCC.Models
         {
             return View(userManager.Users);
         }
-       
+       public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+ 
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email,
+                string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail
+                    = await userValidator.ValidateAsync(userManager, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager,
+                        user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user,
+                            password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if ((validEmail.Succeeded && validPass == null)
+                        || (validEmail.Succeeded
+                        && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
     }
 }

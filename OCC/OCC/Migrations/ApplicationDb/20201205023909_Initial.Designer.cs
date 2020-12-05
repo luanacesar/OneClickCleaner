deb3e2abd1964677d0d80fb912df3fdfc1ffd71f
Luana Cesar de Albuquerque Tavares <lcesarde@my.centennialcollege.ca>
2020-12-05 00:41:12 -0500

    - create Edit button and a few of pages

--- OCC/OCC/Migrations/ApplicationDb/20201205023909_Initial.Designer.cs
+++ OCC/OCC/Migrations/ApplicationDb/20201205023909_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OCC.Models;
 
-namespace OCC.Migrations
+namespace OCC.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201205023909_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
